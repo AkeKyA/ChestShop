8e6ebdaa47f51d7b47bd9e23bfb0884df1ec8910
jasonwynn10 <jasonwynn10@gmail.com>
2016-05-26 13:39:34 -0400

    Update ChestShop.php

--- src/ChestShop/ChestShop.php
+++ src/ChestShop/ChestShop.php
@@ -7,13 +7,26 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as TF;
 
 class ChestShop extends PluginBase {
+    public $moneyManager;
     public function onEnable() {
         if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder());
+        if($this->getServer()->getServer()->getPluginManager()->getPlugin("PocketMoney") !== null) {
+            $this->moneyManager = $this->getServer()->getPluginManager()->getPlugin("PocketMoney");
+            $this->getLogger()->info("Money Manager set to PocketMoney");
+        }elseif($this->getServer()->getPluginManager()->getPlugin("EcomomyAPI")) {
+            $this->moneyManager = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+            $this->getLogger()->info("Money Manager set to EconomyAPI");
+        }else{
+            //
+        }
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this, new DatabaseManager($this->getDataFolder() . 'ChestShop.sqlite3')), $this);
-        $this->getLogger->notice(TF::GREEEN."Disabled!");
+        $this->getLogger->notice(TF::GREEEN."Enabled!");
     }
     public function onDisable() {
         $this->getLogger->notice(TF::GREEEN."Disabled!");
+    }
+    public function getMoneyManager() {
+        return $this->moneyManager;
     }
     public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
         if(strtolower($command) === "id") {
