f20410da9abc5c2241872c2fc9fa6070b2c76731
jasonwynn10 <jasonwynn10@gmail.com>
2016-05-26 13:29:10 -0400

    Update ChestShop.php

--- src/ChestShop/ChestShop.php
+++ src/ChestShop/ChestShop.php
@@ -1,54 +1,37 @@
 <?php
-
 namespace ChestShop;
 
-use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
 
-class ChestShop extends PluginBase
-{
-    public function onLoad()
-    {
-    }
-
-    public function onEnable()
-    {
+class ChestShop extends PluginBase {
+    public function onEnable() {
         if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder());
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this, new DatabaseManager($this->getDataFolder() . 'ChestShop.sqlite3')), $this);
+        $this->getLogger->notice(TF::GREEEN."Disabled!");
     }
-
-    public function onDisable()
-    {
+    public function onDisable() {
+        $this->getLogger->notice(TF::GREEEN."Disabled!");
     }
-
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args)
-    {
-        switch ($command->getName()) {
-            case "id":
-                $name = array_shift($args);
-                $constants = array_keys((new \ReflectionClass("pocketmine\\item\\Item"))->getConstants());
-                foreach ($constants as $constant) {
-                    if (stripos($constant, $name) !== false) {
-                        $id = constant("pocketmine\\item\\Item::$constant");
-                        $constant = str_replace("_", " ", $constant);
-                        $sender->sendMessage("ID:$id $constant");
-                    }
-                }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        if(strtolower($command) === "id") {
+            if($args[0] === "help") {
+                $sender->sendMessage(TF::YELLOW."/id <Item name>");
                 return true;
-
-            default:
-                return false;
+            }
+            $name = array_shift($args);
+            $constants = array_keys((new \ReflectionClass("pocketmine\\item\\Item"))->getConstants());
+            foreach ($constants as $constant) {
+                if (stripos($constant, $name) !== false) {
+                    $id = constant("pocketmine\\item\\Item::$constant");
+                    $constant = str_replace("_", " ", $constant);
+                    $sender->sendMessage("ID:$id $constant");
+                }
+            }
+            return true;
         }
+        return false;
     }
 }
-
-
-
-
-
-
-
-
-
-
